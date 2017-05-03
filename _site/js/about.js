792c1866cfc5e50e66da585ec57849d352e907cc
sagarmunjal <sagarmunjal@gmail.com>
2017-05-03 13:36:27 +0530

    changes pushed

---
+++ _site/js/about.js
@@ -0,0 +1,45 @@
+function output(inp) {
+    document.body.appendChild(document.createElement('pre')).innerHTML = inp;
+}
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
+var obj = {
+    me:{
+        "about": {
+            "name": "sagarmunjal",
+            "nationality": "indian",
+        },
+        "occupation": {
+            "role" : "JavaScriptTrainer",
+            "experience": "3",
+            "curriculumURL":"http://bit.ly/2pWHyxy"
+        },
+        "interests": ["reading","calisthenics","photography","writing"],
+        "profiles":{
+            "twitter":"@munjalsagar",
+            "facebook":"fb.com/munjal27",
+            "github":"https://github.com/sagarmunjal"
+        }
+    }
+};
+var str = JSON.stringify(obj, undefined, 4);
+
+output(syntaxHighlight(str));
